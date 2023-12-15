8fa4efac7bdca8c457ede7e34a7839ee3a9a00a5
anton_penkov <anton.penkov1@gmail.com>
2023-12-15 12:23:19 +0100

    Initial Commit

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Антон Пеньков on 15.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
